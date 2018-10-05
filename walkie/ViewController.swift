cd6b8db7d46ad12c8f5a1695303d9287a95e487d
Eldar Goloviznin <eldargoloviznin@eldars-imac.local>
2018-10-05 21:17:50 +0300

    Initial Commit

---
+++ walkie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  walkie
+//
+//  Created by Eldar Goloviznin on 05/10/2018.
+//  Copyright © 2018 Eldar Goloviznin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
